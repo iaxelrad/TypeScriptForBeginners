0e2ba7a707b4595fe5f9be1f2bb05bf008f9bf2b
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-03 11:03:25 +0200

    Array Type Inference

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -1,1 +1,4 @@
 "use strict";
+let stringArr = ['one', 'hey', 'Itamar'];
+let guitars = ['strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
---
+++ lesson03/src/main.ts
@@ -0,0 +1,5 @@
+let stringArr = ['one', 'hey', 'Itamar'];
+
+let guitars = ['strat', 'Les Paul', 5150];
+
+let mixedData = ['EVH', 1984, true];
