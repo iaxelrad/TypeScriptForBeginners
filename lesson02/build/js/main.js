99318c582f88bac156ef250722032366ed6c1869
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-03 10:48:50 +0200

    The any type.

--- lesson02/build/js/main.js
+++ lesson02/build/js/main.js
@@ -2,6 +2,9 @@
 let myName;
 let meaningOfLife;
 let isLoading;
+let album;
 myName = 'Itamar';
 meaningOfLife = 42;
 isLoading = true;
+album = 'Van Halen';
+album = true;
--- lesson02/src/main.ts
+++ lesson02/src/main.ts
@@ -1,7 +1,10 @@
 let myName: string;
 let meaningOfLife: number;
 let isLoading: boolean;
+let album: any;
 
 myName = 'Itamar';
 meaningOfLife = 42;
 isLoading = true;
+album = 'Van Halen';
+album = true;
