4554e29f39fbbb7ab155c6fea97ed44f89d5178a
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-03 11:11:59 +0200

    Array Assignment

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -2,3 +2,9 @@
 let stringArr = ['one', 'hey', 'Itamar'];
 let guitars = ['strat', 'Les Paul', 5150];
 let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'Jon';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+stringArr = guitars; //TS doesn't allow this assignment of a string | number union array to string array
+guitars = stringArr; //TS allows this assignment of a string array to a string | number union array
