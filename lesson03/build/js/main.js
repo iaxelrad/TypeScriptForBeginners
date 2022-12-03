a7c705f34c34bebb8a1e7cb3a0e25a120f37dae6
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-03 11:48:14 +0200

    Enums

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -46,3 +46,15 @@ const greetGuitarist = (guitarist) => {
     return 'Hello!';
 };
 console.log(greetGuitarist(jp));
+//Enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime"
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+;
+console.log(Grade.U);
--- lesson03/src/main.ts
+++ lesson03/src/main.ts
@@ -67,3 +67,16 @@ const greetGuitarist = (guitarist: Guitarist) => {
 };
 
 console.log(greetGuitarist(jp));
+
+//Enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime"
+
+enum Grade {
+  U = 1,
+  D, 
+  C, 
+  B, 
+  A
+};
+
+console.log(Grade.U);
