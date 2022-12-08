837953cde9959ddcfc72438bd18ed79815a1cff8
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-08 22:18:36 +0200

    Solution 2: Type Assertion

--- lesson05/build/js/copyright.js
+++ lesson05/build/js/copyright.js
@@ -5,11 +5,16 @@
 // year.setAttribute("datetime", thisYear);
 // year.textContent = thisYear;
 //1st variations
-let year;
-year = document.getElementById("year");
-let thisYear;
-thisYear = new Date().getFullYear().toString();
-if (year) {
-    year.setAttribute("datetime", thisYear);
-    year.textContent = thisYear;
-}
+// let year: HTMLElement | null;
+// year = document.getElementById("year");
+// let thisYear: string;
+// thisYear = new Date().getFullYear().toString();
+// if(year) {
+//   year.setAttribute("datetime", thisYear);
+//   year.textContent = thisYear;
+// }
+//2nd variation
+const year = document.getElementById("year");
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
--- lesson05/src/copyright.ts
+++ lesson05/src/copyright.ts
@@ -5,11 +5,17 @@
 // year.textContent = thisYear;
 
 //1st variations
-let year: HTMLElement | null;
-year = document.getElementById("year");
-let thisYear: string;
-thisYear = new Date().getFullYear().toString();
-if(year) {
-  year.setAttribute("datetime", thisYear);
-  year.textContent = thisYear;
-}
+// let year: HTMLElement | null;
+// year = document.getElementById("year");
+// let thisYear: string;
+// thisYear = new Date().getFullYear().toString();
+// if(year) {
+//   year.setAttribute("datetime", thisYear);
+//   year.textContent = thisYear;
+// }
+
+//2nd variation
+const year = document.getElementById("year") as HTMLSpanElement;
+const thisYear: string = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
