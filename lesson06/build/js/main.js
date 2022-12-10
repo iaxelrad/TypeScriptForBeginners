8cdedfe23e1c550b13e4d5452bd9dd8300cd13bb
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 09:34:05 +0200

    Basic Class

--- lesson06/build/js/main.js
+++ lesson06/build/js/main.js
@@ -1,1 +1,6 @@
 "use strict";
+class Coder {
+    constructor(name, music, age, lang) {
+        this.name = name;
+    }
+}
---
+++ lesson06/src/main.ts
@@ -0,0 +1,7 @@
+class Coder {
+  name: string;
+
+  constructor(name: string, music: string, age: number, lang: string) {
+    this.name = name;
+  }
+}
