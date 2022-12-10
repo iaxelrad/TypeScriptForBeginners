497948ff5f03ce0950f819bcc98ba78bbeea5751
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 09:55:54 +0200

    Private and Protected Examples

--- lesson06/build/js/main.js
+++ lesson06/build/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 class Coder {
-    constructor(name, music, age, lang) {
+    constructor(name, music, age, lang = 'Typescript') {
         this.name = name;
         this.music = music;
         this.age = age;
@@ -10,4 +10,11 @@ class Coder {
         this.age = age;
         this.lang = lang;
     }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
 }
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+console.log(Dave.age);
+console.log(Dave.lang);
--- lesson06/src/main.ts
+++ lesson06/src/main.ts
@@ -5,11 +5,20 @@ class Coder {
     public readonly name: string,
     public music: string,
     private age: number,
-    protected lang: string
+    protected lang: string = 'Typescript'
   ) {
     this.name = name;
     this.music = music;
     this.age = age;
     this.lang = lang;
   }
+
+  public getAge() {
+    return `Hello, I'm ${this.age}`;
+  }
 }
+
+const Dave = new Coder('Dave', 'Rock', 42);
+console.log(Dave.getAge());
+console.log(Dave.age);
+console.log(Dave.lang);
