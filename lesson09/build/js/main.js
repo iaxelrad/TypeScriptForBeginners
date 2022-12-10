69896f3af7e344b314d142c699ab0240264a0155
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 13:37:54 +0200

    Pick & Omit Utility Types

--- lesson09/build/js/main.js
+++ lesson09/build/js/main.js
@@ -31,3 +31,11 @@ const gradeData = {
     Sarah: { assign1: 85, assign2: 93 },
     Kelly: { assign1: 76, assign2: 15 },
 };
+const score = {
+    studentId: 'k123',
+    grade: 85,
+};
+const preview = {
+    studentId: 'k123',
+    title: 'Final Project',
+};
