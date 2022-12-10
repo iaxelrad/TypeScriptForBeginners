e1901c6d49ecc1276f212a9edff5a7ee4e56c59a
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 13:23:05 +0200

    Partial Utility Type

--- lesson09/build/js/main.js
+++ lesson09/build/js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+// Utility Types
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
+};
+const assign1 = {
+    studentId: 'conpsci123',
+    title: 'Final Project',
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded = updateAssignment(assign1, { grade: 95 });
