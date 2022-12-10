3039edf720e2100bfb1eb24106ad769eab40df92
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 19:53:51 +0200

    useState

--- lesson13_react_hooks/src/App.tsx
+++ lesson13_react_hooks/src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+
+interface User {
+  id: number;
+  username: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [users, setUsers] = useState<User[] | null>(null);
 
   return <div className="App"></div>;
 }
