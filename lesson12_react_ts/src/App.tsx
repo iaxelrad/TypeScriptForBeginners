a4ed3063b6c71673c575e1d5f767a3e5f44cfe52
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 17:23:07 +0200

    Passing down setState & children

--- lesson12_react_ts/src/App.tsx
+++ lesson12_react_ts/src/App.tsx
@@ -1,13 +1,16 @@
 import Heading from './components/Heading';
 import { Section } from './components/Section';
 import Counter from './components/Counter';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState<number>(1);
+
   return (
     <>
       <Heading title="Hello" />
       <Section title="Different Title">This is my Section</Section>
-      <Counter />
+      <Counter setCount={setCount}>Count is {count}</Counter>
     </>
   );
 }
