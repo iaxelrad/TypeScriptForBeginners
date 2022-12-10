aa20b9e9ca38ce813b06cd254d32466eb6d055e1
Itamar Axelrad <iaxelrad@gmail.com>
2022-12-10 17:32:46 +0200

    Generic List Example

--- lesson12_react_ts/src/App.tsx
+++ lesson12_react_ts/src/App.tsx
@@ -2,6 +2,7 @@ import Heading from './components/Heading';
 import { Section } from './components/Section';
 import Counter from './components/Counter';
 import { useState } from 'react';
+import List from './components/List';
 
 function App() {
   const [count, setCount] = useState<number>(1);
@@ -11,6 +12,10 @@ function App() {
       <Heading title="Hello" />
       <Section title="Different Title">This is my Section</Section>
       <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={['coffee', 'tacos', 'code']}
+        render={(item: string) => <span className="bold">{item}</span>}
+      />
     </>
   );
 }
