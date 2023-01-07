7cb3b011777617744b66fb06931382e61e417c2b
Itamar Axelrad <iaxelrad@gmail.com>
2023-01-07 13:30:54 +0200

    Dynamic Images with Vite & React

--- lesson16_react_ts_cart/src/components/Product.tsx
+++ lesson16_react_ts_cart/src/components/Product.tsx
@@ -15,6 +15,11 @@ const Product = ({
   REDUCER_ACTIONS,
   inCart,
 }: PropsType): ReactElement => {
+  // const img: string = require(`../images/${product.sku}.jpg`); // Won't work with Vite
+  const img: string = new URL(`../images/${product.sku}.jpg`, import.meta.url)
+    .href; //Will work with Vite & create-react-app
+  console.log(img);
+
   return <div>Product</div>;
 };
 export default Product;
